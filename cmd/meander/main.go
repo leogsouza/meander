695cb2da7a3fffc88f0efd9a88342d540bdd6fc3
Leonardo Souza <leogsouza@gmail.com>
2018-05-20 23:10:31 -0300

    Finish application

--- cmd/meander/main.go
+++ cmd/meander/main.go
@@ -4,17 +4,59 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/leogsouza/meander"
 )
 
 func main() {
 	meander.APIKey = os.Getenv("GOOGLE_PLACES_API_KEY")
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter,
+		r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+
+		var err error
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+
+	}))
 
 	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+
+		f(w, r)
+	}
 }
 
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
